c069ab4a1c20329a25e64344308279bc57208352
Jakob Demler <jdemler@curry-software.com>
2016-10-05 22:56:29 +0200

    implemented collection generator

--- collection-generator/src/main.rs
+++ collection-generator/src/main.rs
@@ -40,18 +40,40 @@ fn main() {
         return;
     }
     let file = File::create(matches.free[0].clone()).unwrap();
-    generate_collection(docs.unwrap(), len.unwrap(), voc, file);
-
-}
-
-fn generate_collection<W: Write>(docs: usize, len: usize, voc: Option<usize>, output: W) {
+    let docs = docs.unwrap();
+    let len = len.unwrap();
     let voc = voc.unwrap_or(voc_size(45, 0.5, docs * len));
     println!("Generating a collection with {} documents each {} terms long. This collection \
-              contains {} total terms and sums up to {} in total",
+              contains {} total terms and sums up to {} in total writing the results in {}",
              docs,
              len,
              voc,
-             fmt_bytes(docs * len * 8));
+             fmt_bytes(docs * len * 8),
+             matches.free[0].clone());
+    println!("Continue? (y/n)");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    if !input.starts_with("y") {
+        return;
+    }
+    generate_collection(docs, len, voc, file);
+
+}
+
+fn generate_collection<W: Write>(docs: usize, len: usize, voc: usize, mut output: W) {
+
+    let mut rng = ZipfGenerator::new(voc);
+    for i in 0..docs {
+        output.write(&rng.take(len)
+                .map(|t| vbyte_encode(t))
+                .flat_map(|tbytes| tbytes)
+                .collect::<Vec<_>>())
+            .unwrap();
+        output.write(&[0]).unwrap();
+        print!("\r{} of {} documents generated!", i + 1, docs);
+        std::io::stdout().flush().unwrap();
+    }
+    println!("\nDone!");
 }
 
 fn fmt_bytes(bytes: usize) -> String {
@@ -81,6 +103,21 @@ fn voc_size(k: usize, b: f64, tokens: usize) -> usize {
     ((k as f64) * (tokens as f64).powf(b)) as usize
 }
 
+/// Encode an usigned integer as a variable number of bytes
+pub fn vbyte_encode(mut number: usize) -> Vec<u8> {
+    let mut result = Vec::new();
+    loop {
+        result.insert(0, (number % 128) as u8);
+        if number < 128 {
+            break;
+        } else {
+            number /= 128;
+        }
+    }
+    let len = result.len();
+    result[len - 1] += 128;
+    result
+}
 
 #[derive(Clone)]
 pub struct ZipfGenerator {
@@ -107,7 +144,7 @@ impl ZipfGenerator {
     }
 }
 
-impl Iterator for ZipfGenerator {
+impl<'a> Iterator for &'a mut ZipfGenerator {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
