928c4858633a8c45c038d54e2fe51fdabdd8d4f7
Jakob Demler <jdemler@curry-software.com>
2016-12-06 16:07:47 +0100

    Build Menu structure and support for positional queries

--- testbench/src/main.rs
+++ testbench/src/main.rs
@@ -4,10 +4,12 @@ extern crate time;
 use std::env;
 use std::fs::File;
 use std::io::{Bytes, Read, Write};
+use std::str::FromStr;
 use std::iter::Peekable;
 
 use perlin::storage::RamStorage;
-use perlin::index::boolean_index::{BooleanIndex, IndexBuilder};
+use perlin::index::Index;
+use perlin::index::boolean_index::{BooleanIndex, IndexBuilder, QueryBuilder};
 
 macro_rules! try_option{
     ($operand:expr) => {
@@ -24,6 +26,13 @@ fn print_usage(program: &str) {
     let brief = format!("Usage: {} [COLLECTION_FILE]", program);
     println!("{}", brief);
 
+}
+
+fn get_next_line_trimmed() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input = input.trim().to_string();
+    input
 }
 
 fn main() {
@@ -59,16 +68,96 @@ fn main() {
     println!("At a rate of {}/s",
              fmt_bytes((docs * len * 8 * 1000) /
                        start.to(time::PreciseTime::now()).num_milliseconds() as usize));
-    println!("{}", index.document_count());
+    main_menu(&index);
+}
 
+fn main_menu(index: &BooleanIndex<usize>) {
+    println!("Main Menu: What do you want to do? (h/? for help)");
+    loop {
+        let input = get_next_line_trimmed();
+        match input.as_str() {
+            "?" | "h" => main_menu_help(),
+            "1" => query_menu(index),
+            "q" => break,
+            _ => println!("Sorry. I did not understand {}!", input),
+        }
+    }
+}
+
+fn main_menu_help() {
+    println!("Main Menu Usage:");
+    println!("\th\tprint this message");
+    println!("\t?\tprint this message");
+    println!("");
+    println!("\t1\tnavigate to query menu");
+    println!("");
+    println!("\tq\tquit");
+}
+
+fn query_menu(index: &BooleanIndex<usize>) {
+    println!("Query Menu: What do you want to do? (h/? for help)");
+    loop {
+        let input = get_next_line_trimmed();
+        match input.as_str() {
+            "?" | "h" => query_menu_help(),
+            "1" => positional_query(index),
+            "q" => break,
+            _ => println!("Sorry. I did not understand {}!", input),
+        }
+    }
+}
+
+fn query_menu_help() {
+    println!("Query Menu Usage:");
+    println!("\th\tprint this message");
+    println!("\t?\tprint this message");
+    println!("");
+    println!("\t1\texecute positional query");
+    println!("");
+    println!("\tq\tback to main menu");
+}
+
+fn positional_query(index: &BooleanIndex<usize>) {
+    println!("Please state your information need! (h/? for help)");
+    loop {
+        let input = get_next_line_trimmed();
+        match input.as_str() {
+            "?" | "h" => {
+                positional_query_help();
+                return;
+            }
+            "q" => return,
+            _ => {}
+        }
+        let terms = input.split_whitespace().map(|t| usize::from_str(t)).collect::<Vec<_>>();
+        if terms.iter().any(|t| t.is_err()) {
+            println!("Parsing Error! Try again!");
+        } else {
+            let query =
+                QueryBuilder::in_order(terms.into_iter().map(|t| Some(t.unwrap())).collect::<Vec<_>>())
+                .build();
+            let start = time::PreciseTime::now();
+            let query_result = index.execute_query(&query);
+            println!("Looked up query terms! (took {}µs)", start.to(time::PreciseTime::now()).num_microseconds().unwrap());
+            let result_count = query_result.count();
+            println!("Query returned {} documents and took {}ms to evaluate!", result_count, start.to(time::PreciseTime::now()).num_milliseconds());
+        }
+
+    }
 
 }
 
-fn index<R: Read>(collection: CollectionIterator<R>) -> BooleanIndex<usize>{
-    IndexBuilder::<_, RamStorage<_>>::new()
+fn positional_query_help() {
+    println!("Positional Query Usage:");
+    println!("Term (SPACE Term)*");
+    println!("I.e. '1 2 3' queries for the sequence '1 2 3'");
+}
+
+fn index<R: Read>(collection: CollectionIterator<R>) -> BooleanIndex<usize> {
+    IndexBuilder::<_, RamStorage<_>, RamStorage<_>>::new()
         .create(collection.map(|v| v.into_iter()))
         .unwrap()
- 
+
 }
 
 fn fmt_bytes(bytes: usize) -> String {
